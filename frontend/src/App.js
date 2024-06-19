2d1b5d2d76eefb0fbfbd5a549c50c036261adc92
im-e <immie@riseup.net>
2024-06-19 10:35:09 +0100

    got started on react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    players: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/api/players');
+    const body = await response.json();
+    this.setState({players: body});
+  }
+
+  render()
+  {
+    const {players} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <div className="App-intro">
+              <h2>Players</h2>
+              {players.map(player =>
+                  <div key={player.id}>
+                    {player.username} ({player.elo})
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
