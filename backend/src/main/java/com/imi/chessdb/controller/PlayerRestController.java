fad47a4b6e2de8e8874410e833a571cd3c2ef618
im-e <immie@riseup.net>
2024-06-19 10:35:43 +0100

    added rest controller for frontend

---
+++ backend/src/main/java/com/imi/chessdb/controller/PlayerRestController.java
@@ -0,0 +1,59 @@
+package com.imi.chessdb.controller;
+
+import com.imi.chessdb.model.entities.Player;
+import com.imi.chessdb.model.repositories.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/players")
+public class PlayerRestController {
+
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerRestController(final PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    @GetMapping
+    public List<Player> getAllPlayers() {
+        return playerRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Player getPlayerById(@PathVariable Integer id) {
+        return playerRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    @PostMapping
+    public ResponseEntity createPlayer(@RequestBody Player player) throws URISyntaxException {
+        Player savedPlayer = playerRepository.save(player);
+        return ResponseEntity.created(new URI("/api/players/" + savedPlayer.getId())).body(savedPlayer);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updatePlayer(@PathVariable Integer id, @RequestBody Player player) {
+        Player currentPlayer = playerRepository.findById(id).orElseThrow(RuntimeException::new);
+        currentPlayer.setUsername(player.getUsername());
+        currentPlayer.setPassword(player.getPassword());
+        currentPlayer.setElo(player.getElo());
+        currentPlayer = playerRepository.save(player);
+
+        return ResponseEntity.ok(currentPlayer);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletePlayer(@PathVariable Integer id) {
+        playerRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+
+}
