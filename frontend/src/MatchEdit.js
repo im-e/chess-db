060c2b0986668f499387065fff08bf422ad6d60e
im-e <immie@riseup.net>
2024-06-24 14:26:58 +0100

    started matchEdit

---
+++ frontend/src/MatchEdit.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
+import AppNavbar from './AppNavbar';
+
+class PlayerEdit extends Component {
+
+    emptyItem = {
+        username: '',
+        elo: ''
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            item: this.emptyItem
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    async componentDidMount() {
+        if (this.props.match.params.id !== 'new') {
+            const match = await (await fetch(`/players/${this.props.match.params.id}`)).json();
+            this.setState({item: match});
+        }
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        let item = {...this.state.item};
+        item[name] = value;
+        this.setState({item});
+    }
+
+    async handleSubmit(event) {
+        event.preventDefault();
+        const {item} = this.state;
+
+        await fetch('/players' + (item.id ? '/' + item.id : ''), {
+            method: (item.id) ? 'PUT' : 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(item),
+        });
+        this.props.history.push('/players');
+    }
+
+
+    render() {
+        const {item} = this.state;
+        const title = <h2>{item.id ? 'Edit Match' : 'Add Match'}</h2>;
+
+        return <div>
+            <AppNavbar/>
+            <Container>
+                {title}
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="name">Username</Label>
+                        <Input type="text" name="username" id="username" value={item.username || ''}
+                               onChange={this.handleChange} autoComplete="username"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="elo">Elo</Label>
+                        <Input type="text" name="elo" id="elo" value={item.elo || ''}
+                               onChange={this.handleChange} autoComplete="elo"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Button color="primary" type="submit">Save</Button>{' '}
+                        <Button color="secondary" tag={Link} to="/players">Cancel</Button>
+                    </FormGroup>
+                </Form>
+            </Container>
+        </div>
+    }
+
+}
+export default withRouter(PlayerEdit);
